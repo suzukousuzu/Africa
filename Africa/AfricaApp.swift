bd596718493d9ff9ee700634eef5234f0a4862b7
Kota Suzuki <addressuzuki@gmail.com>
2024-02-13 19:58:37 +0900

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by 鈴木航太 on 2024/02/13.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
